38056d6e56ad16d90c8c61d722171ba696ab11f4
Stewart B <stew822@gmail.com>
2020-07-12 07:04:49 +0000

    set up repo

--- source/ContentType.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-abstract class ContentType {
-    public function head() {
-        echo "";
-    }
-    abstract public function editor( $name, $location ); 
-}
--- source/zest.php
+++ source/zest.php
@@ -4,31 +4,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-set_include_path( '/workspace/zest-cms/source' );
+set_include_path( '/workspace/zest-library-php/source' );
 
-require_once( "Pages/Editor.php" );
-require_once( "Pages/PageNotFound.php");
-if( isset($_GET['page']) ) { 
-    $slug = $_GET['page'];
+class Zest {
+
+    public $location;
+
+    public function __construct( $location ) {
+        $this->location = $location;
+    }
+
+    function getItems( $subdirectory = "" ) {
+        
+    }
 }
-else {
-    $slug = "editor";
-}
-
-if( !isset($_GET['item']) ) { 
-    $_GET['item'] = "../example.collection";
-}
-
-$pages = [];
-$pages["editor"] = new Pages\Editor();
-
-if( isset($pages[$slug] ) ) {
-    $page = $pages[$slug];
-}
-else {
-    $page = new Pages\PageNotFound();
-}
-
-include( "theme/theme.php" );
-
-
